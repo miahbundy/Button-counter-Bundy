333942bf7fd94236fee74f1be98dc31663663b03
miahbundy <bundyme@g.cofc.edu>
2017-02-02 01:14:28 -0500

    does the thing

--- app/src/main/java/com/introtoandroid/button_counter_bundy/MainActivity.java
+++ app/src/main/java/com/introtoandroid/button_counter_bundy/MainActivity.java
@@ -6,12 +6,16 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView showValue;
     int counter = 0;
     int initSize;
+    Integer num;
+
+    //warnings
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,4 +41,19 @@ public class MainActivity extends AppCompatActivity {
         showValue.setText(Integer.toString(counter));
     }
 
+
+    @Override
+    public void onSaveInstanceState(Bundle outState){
+        super.onSaveInstanceState(outState);
+        //Log.i("Instance state", "onSaveInstanceState");
+        outState.putInt("count", counter);
+    }
+
+    @Override
+    public void onRestoreInstanceState(Bundle savedInstanceState){
+        super.onRestoreInstanceState(savedInstanceState);
+        //Log.i("Instance state", "onSaveInstanceState");
+        num = savedInstanceState.getInt("count");
+        showValue.setText(num.toString());
+    }
 }
